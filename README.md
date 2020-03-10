63fffade9cf9c60a8710e37349a35ba06dff1373
Sujit Malde <samman30@live.com>
2020-03-10 18:16:33 +0000

    Improved Python and README.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # letter_sort
 A small program to verify the results of a mathematical probability problem.
+
+> You are in the process of applying to five financial services companies.
+> You have written cover letters for each, all of which are clearly marked,
+> along with five clearly marked envelopes addressed to each company.
+> You have not placed the letters into the envelope because you have gone to greet
+> your family who have just arrived for a dinner party. Unfortunately, your young
+> nephew manages to get into your study and, in an attempt to help you, decides to
+> put each letter inside an envelope. The problem is, he cannot read yet and so
+> has placed letters randomly into envelopes.
+> What is the probability that every letter has found its way into the incorrect
+> envelope?
+
+This problem can, of course, be solved mathematically. We attempted to solve it
+and eventually Charlotte managed to find the solution. However, we weren't sure
+if this was correct or not, so we decided to find the percentage of every
+letter placement being incorrect out of a large number of samples.
+
+This isn't complicated code. We made this in about an hour. I uploaded it to
+Github to teach Charlotte the principles of version control. She wrote basically
+all the code, and I improved it (improving structure).
--- letter.py
+++ letter.py
@@ -1,56 +1,41 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Mar  8 19:55:29 2020
-
-@author: charl
+@authors: charl and suj
 """
+# Importing modules
 import matplotlib.pyplot as plt
 import random
-random.seed()
 from matplotlib import style
 
-
-style.use('ggplot')
-letters=int(input("How many letters are there? "))
-tests=int(input("How many tests would you like to perform? Minimum is 1000. "))
-if tests < 1000:
-    tests = 1000
-print("You have chosen to perform",tests,"tests on",letters,"letters.")
-wrong = 0
-for k in range(tests):
-    n = []
-    m = []
-    for i in range(1,letters+1):
-        n.append(i)
-        m.append(i)
-    random.shuffle(n)
-    count = 0
-    for j in range(letters):
-        if n[j] == m[j]:
-           count = count + 1
-    if count == 0:
-        wrong = wrong + 1
-print("The probablity that all the letters are wrong is", (wrong/tests))
-letters=int(input("How many letters are there? "))
-x = list(range(100,10000,100))
-y = []
-for tests in x:
+def shuffler(tests,letters):
     wrong = 0
+    n = list(range(letters))
+    m = list(range(letters))
     for k in range(tests):
-        n = []
-        m = []
-        for i in range(1,letters+1):
-            n.append(i)
-            m.append(i)
         random.shuffle(n)
         count = 0
         for j in range(letters):
             if n[j] == m[j]:
-               count = count + 1
+               count += 1
         if count == 0:
-           wrong = wrong + 1
-    y.append(wrong/tests)
-plt.plot(x,y,'r')
-plt.xlabel('Number of trials',fontsize=14)
-plt.ylabel('Probability value',fontsize=14)
-plt.show()
+           wrong += 1
+    return wrong
+
+def tester(wrong):
+    random.seed()
+    style.use('ggplot')
+    letters=int(input("How many letters are there? "))
+    x = list(range(100,10000,10))
+    y = []
+    for tests in x:
+        wrong = shuffler(tests,letters)
+        y.append(wrong/tests)
+    plt.plot(x,y,'r')
+    plt.xlabel('Trial number',fontsize=14)
+    plt.ylabel('Probability',fontsize=14)
+    plt.xscale("log")
+    plt.show()
+
+if __name__ == "__main__":
+    tester(0)
